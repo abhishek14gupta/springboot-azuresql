9e7cd2f26811e4996809657b217e7b40962ac8cf
abhishekgupta14 <abhishek.gupta14@nagarro.com>
2023-04-04 18:12:20 +0530

    azureassign

---
+++ src/main/java/com/nagarro/springazure/Employee.java
@@ -0,0 +1,53 @@
+package com.nagarro.springazure;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String dept;
+    private long salary;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public Employee(String name, String dept, long salary) {
+		super();
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+    
+    
+}
